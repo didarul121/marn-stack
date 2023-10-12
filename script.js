27ac25ae260f5db8c45dda28ca94a2cf07251744
didarul121 <didarul7755@gmail.com>
2023-10-13 01:31:08 +0600

    assignment

---
+++ script.js
@@ -0,0 +1,6 @@
+humburger = document.querySelector(".humburger");
+    humburger.onclick = function(){
+        navBar = document.querySelector(".navbars");
+        navBar.classList.toggle('active')
+    }
+
